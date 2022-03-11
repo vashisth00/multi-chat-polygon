c5dd21a54d03636487be017922546ff29181dcd2
Vashisth Bhushan <vashisthbhushan@gmail.com>
2022-03-11 23:58:12 +0530

    comments + test

--- contracts/Comments.sol
+++ contracts/Comments.sol
@@ -4,21 +4,33 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Comments {
-  // Exposed data structure
-  struct Comment {
-    uint32 id;
-    string topic;
-    address creator_address;
-    string message;
-    uint256 created_at;
-  }
-       
-  // Notify users that a comment was added 
-  event CommentAdded(Comment comment);
+    struct Comment {
+        uint32 id;
+        string topic;
+        address creator_address;
+        string message;
+        uint created_at;
+    }
 
-  // Fetch a list of comments for a topic 
-  function getComments(string calldata topic) public view returns(Comment[] memory) {}
+    uint32 private idCounter;
+    mapping(string => Comment[]) private commentsByTopic;
+        
+    event CommentAdded(Comment comment);
 
-  // Persist a new comment
-  function addComment(string calldata topic, string calldata message) public {}
+    function getComments(string calldata topic) public view returns(Comment[] memory) {
+       return commentsByTopic[topic];
+    }
+
+    function addComment(string calldata topic, string calldata message) public {
+        Comment memory comment = Comment({
+            id: idCounter,
+            topic: topic,
+            creator_address: msg.sender,
+            message: message,
+            created_at: block.timestamp
+        });
+        commentsByTopic[topic].push(comment);
+        idCounter++;
+        emit CommentAdded(comment);
+    }
 }
