c55975e1a5f9687b87d2ee56ef4f6276f9ccc0f6
Vashisth Bhushan <vashisthbhushan@gmail.com>
2022-03-12 01:06:52 +0530

    Comment Editor

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner, Stack, Center } from "@chakra-ui/react";
 import useComments from "../hooks/useComments";
+import Comment from "./Comment";
 
 interface CommentsProps {
   topic: string;
@@ -9,7 +10,20 @@ interface CommentsProps {
 const Comments: React.FunctionComponent<CommentsProps> = ({ topic }) => {
   const query = useComments({ topic });
 
-  return <Box as="pre">{JSON.stringify(query.data, null, 2)}</Box>;
+  return (
+    <Box>
+      {query.isLoading && (
+        <Center p={8}>
+          <Spinner />
+        </Center>
+      )}
+      <Stack spacing={4}>
+        {query.data?.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Comments;
